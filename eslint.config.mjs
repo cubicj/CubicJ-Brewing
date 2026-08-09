636e0a0cd7b5d8056a5fcff23aee9de18a63ba17
cubicj <124673057+cubicj@users.noreply.github.com>
2026-08-09 16:05:47 +0900

    chore: remove husky and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,8 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
-
 export default tseslint.config(
 	js.configs.recommended,
 	...tseslint.configs.recommended,
-	prettier,
 	{
 		languageOptions: {
 			parserOptions: {
