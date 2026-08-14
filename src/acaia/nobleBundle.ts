a16494c5bd77beb7d9bc915e9772d2c53ae07cd2
cubicj <124673057+cubicj@users.noreply.github.com>
2026-08-14 20:29:10 +0900

    chore: drop v prefix from noble bundle release url

--- src/acaia/nobleBundle.ts
+++ src/acaia/nobleBundle.ts
@@ -3,5 +3,5 @@ export const NOBLE_BUNDLE_SHA256 = '93385e24c453d4fde5182da0e7442df450a19337bf15
 export const NOBLE_BUNDLE_ASSET = 'noble.tar.gz';
 
 export function nobleBundleUrl(pluginVersion: string): string {
-	return `https://github.com/cubicj/CubicJ-Brewing/releases/download/v${pluginVersion}/${NOBLE_BUNDLE_ASSET}`;
+	return `https://github.com/cubicj/CubicJ-Brewing/releases/download/${pluginVersion}/${NOBLE_BUNDLE_ASSET}`;
 }
